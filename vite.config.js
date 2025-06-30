057e66c0338177259337479c9ba6c9fcc8db25c6
ShahFaisalFarook <shahfaisalfarook@gmail.com>
2025-06-30 12:30:37 +0530

    Added base config for GitHub Pages in Vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/dialUsCreations/', // 👈 required
+  base: '/dialUs-Live/',
   plugins: [react()],
 })
